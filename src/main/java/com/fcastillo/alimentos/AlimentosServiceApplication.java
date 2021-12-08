e2c3e10026954f0a9ff628083c30e6c3b14422df
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-12-08 11:40:28 -0300

    commit inicial

---
+++ src/main/java/com/fcastillo/alimentos/AlimentosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fcastillo.alimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcastillo/alimentos/AlimentosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcastillo.alimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
